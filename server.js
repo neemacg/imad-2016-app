201657ecd14f988fd4be882a5b68706c86ebdfd2
neemacg <neemacg87@gmail.com>
2016-10-06 03:15:42 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-1', function (req, res) {
-  res.sendFile('Article one requested and will be served here'));
+  res.sendFile('Article one requested and will be served here');
 });
 app.get('/article-2', function (req, res) {
   res.sendFile('Article two requested and will be served here');
