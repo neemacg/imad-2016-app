a5ae11637ddc4113b83035a6081bc826503db1b3
neemacg <neemacg87@gmail.com>
2016-10-06 03:14:35 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-1', function (req, res) {
+  res.sendFile('Article one requested and will be served here'));
+});
+app.get('/article-2', function (req, res) {
+  res.sendFile('Article two requested and will be served here');
+});
+app.get('/article-3', function (req, res) {
+  res.sendFile('Article three requested and will be served here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
