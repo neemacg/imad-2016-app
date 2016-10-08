c0c6f093792c8d42594b08d2f354322d9de339a8
neemacg <neemacg87@gmail.com>
2016-10-08 17:30:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,17 @@ button.onclick=function()
    request.open('GET',"http://neemacg.imad.hasura-app.io/counter",true);
    request.send(null);
 }
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function()
+{
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i=names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('nameslist');
+    ul.innerHTML=list;
+}
