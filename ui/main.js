84aefcc4317f8ab2640491a254e63f99d42bbb43
neemacg <neemacg87@gmail.com>
2016-10-08 17:32:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,6 @@ submit.onclick=function()
     {
         list+='<li>'+names[i]+'</li>';
     }
-    var ul=document.getElementById('nameslist');
+    var ul=document.getElementById('namelist');
     ul.innerHTML=list;
 }
