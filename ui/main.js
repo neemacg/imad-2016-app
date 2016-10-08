038e6e728fb476cd2ad8387ef6d79c5b9cf95c1d
neemacg <neemacg87@gmail.com>
2016-10-08 21:54:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,9 +18,10 @@ button.onclick=function()
    request.send(null);
 }
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+    var name=nameInput.value;
     var request=new XMLHttpRequest();
     request.onreadystatechange = function(){
         if (request.readyState === XMLHttpRequest.DONE)
