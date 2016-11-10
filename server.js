6b780f1d28d9658df9ce9d660b59a8a73bd1b67c
neemacg <neemacg87@gmail.com>
2016-11-10 22:01:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/register', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'register.html'));
+});
+
 var counter=0;
 app.get('/counter',function(req,res)
 {
