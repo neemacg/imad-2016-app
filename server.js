286e20eb6281ab34fbb254e4829a5926ec17789b
neema <neemacg87@gmail.com>
2016-11-12 23:26:15 +0530

    register, article add

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var fileUpload = require('express-fileupload');
+
 
 var app = express();
 app.use(morgan('combined'));
 
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// default options 
+app.use(fileUpload());
 
 // Database connection 
 
@@ -113,6 +119,10 @@ app.get('/register', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'register.html'));
 });
 
+app.get('/addArticle', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'addArticle.html'));
+});
+
 var counter=0;
 app.get('/counter',function(req,res)
 {
@@ -129,10 +139,10 @@ app.get('/submit-name',function(req,res)
 });
 
 
-app.get('/:articleName', function (req, res) {
+/*app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
  res.send(createtemplate(articles[articleName]));
-});
+});*/
 
 
 app.get('/ui/style.css', function (req, res) {
@@ -148,33 +158,88 @@ app.get('/ui/madi.png', function (req, res) {
 
 // Post methods
 
+app.post('/regValidate',function(req,res){
+    var name = req.body.user.name;
+    var email = req.body.user.email;
+    var password = req.body.user.password;
+    
+    // Validate the user
+    res.setHeader('Content-Type', 'application/json');
+    /*pool.query('INSERT INTO "user" (email,pwd,uname) values($1,$2,$3)',[email,password,name],function(err,result){
+    	if(err) console.log(err);
+    	
+    	
+    });*/
+    res.send(JSON.stringify({ status: 'OK'}));
+    
+});
+
+
+
 app.post('/regSubmit',function(req,res){
     var name = req.body.user.name;
     var email = req.body.user.email;
     var password = req.body.user.password;
     
     // Insert into the db
-    
-    pool.query('INSERT INTO user(email,pwd,uname) values($1,$2,$3)',[email,password,name]);
-    
-    pool.connect(function(err, client, done) {
+    res.setHeader('Content-Type', 'application/json');
+    pool.query('INSERT INTO "user" (email,pwd,uname) values($1,$2,$3)',[email,password,name],function(err,result){
+    	if(err) console.log(err);
+    	
+    	
+    });
+    //res.send(JSON.stringify({ status: 'OK'}));
+    /*pool.connect(function(err, client, done) {
       if(err) {
-           res.send(err);
+           console.log(err);
         //return console.error('error fetching client from pool', err);
       }
       client.query('INSERT INTO user(email,pwd,uname) values($1,$2,$3)',[email,password,name],function(err,result){
           if(err){
-              res.send(err);
+              console.log(err);
           }
           else{
-              res.send("Doneee");
+              console.log("Doneee");
           }
       });
-    });
+    });*/
     
     
-    res.send(name + ' ' + email + ' ' + password+" inserted the row");
+    res.redirect('/');
 });
+
+//Add article
+
+app.post('/articleSubmit',function(req,res){
+    
+    var title = req.body.title;
+    var content = req.body.content;
+
+    articleImage = req.files.articleImage;
+    var timeStamp = new Date().getTime();
+    var imageName = './images/'+timeStamp+'_img.jpg';
+    console.log(imageName);
+    articleImage.mv(imageName, function(err) {
+        if (err) {
+        	console.log(err);
+            res.status(500).send(err);
+        }
+        else {
+            console.log("File uploaded  Moved ");
+        }
+    });
+
+    pool.query('INSERT INTO "article" (title,content,image) values($1,$2,$3)',[title,content,timeStamp+'_img.jpg'],function(err,result){
+    	if(err) console.log(err);
+    	
+    	console.log("Query insert ::")
+    });
+
+    res.redirect('/');
+    
+});
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
