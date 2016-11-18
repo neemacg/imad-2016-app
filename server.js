80e1536d9d9884190d69f880daf7a65b4842e224
neemacg <neemacg87@gmail.com>
2016-11-18 22:01:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // default options 
-app.use(fileUpload());
+app.use(fileUpload()); 
 
 // Database connection 
 
