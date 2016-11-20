fef79a60c219d23516bbb2e8b591787bda68f0b1
neema <neemacg87@gmail.com>
2016-11-20 22:18:33 +0530

    updates

--- server.js
+++ server.js
@@ -386,8 +386,10 @@ function getArticle(res,id){
 				  console.log(item);
 				  	  	
 				  	  html_article += '<div class="row rowPad"><div class="col-md-8 col-centered"><div class="media"><div class="">'; 
-				      //html_article +='<img class="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2MDkzNzUiIHk9IjMyIiBzdHlsZT0iZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQ7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9nPjwvc3ZnPg==" alt="...">';
-                	  html_article +='</div><div class="media-body"><h4 class="media-heading">'+item.title+'</h4>'+item.content;
+				      
+                	  html_article +='</div><div class="media-body"><h2 class="media-heading">'+item.title+'</h2>';
+                	  if(item.image) html_article +='<img style="width:20rem" class="media-object" src="'+item.image+'" alt="...">';
+                	  html_article +=item.content;
               		  html_article += '</div></div></div></div>';
               		  
 				});
